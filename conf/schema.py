a76509b9104bf07cc687e4ba0fff31ed0cccd9dd
Jisun Park <fpdldk912@gmail.com>
2020-05-02 22:54:36 +0900

    make Mutation

--- conf/schema.py
+++ conf/schema.py
@@ -3,8 +3,14 @@ import graphene
 import links.schema
 
 
+# inherits the query in links/schema.py
+# to keep every schema isolated in apps
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(links.schema.Mutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- links/schema.py
+++ links/schema.py
@@ -13,4 +13,35 @@ class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
-        return Link.objects.all()
+        return Link.objects.all() # return all the links
+
+
+# defines a mutation class
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # defines the data you can send to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+
+    # the mutation method:
+    # 1. creates a link in the DB
+    # 2. returns CreateLink class with the data just created
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id = link.id,
+            url = link.url,
+            description = link.description,
+        )
+
+
+# creates a mutation class with a field to be resolved
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
