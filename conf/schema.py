15bf42c3763076aa5979f0b20e375b23a207fa86
Jisun Park <fpdldk912@gmail.com>
2020-05-02 23:37:05 +0900

    create and make user query, mutation, and authentication

--- conf/schema.py
+++ conf/schema.py
@@ -1,16 +1,20 @@
 import graphene
+import graphql_jwt # implement JWT TOkens for user authentication
 
 import links.schema
+import users.schema
 
 
 # inherits the query in links/schema.py
 # to keep every schema isolated in apps
-class Query(links.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
 
-class Mutation(links.schema.Mutation, graphene.ObjectType):
-    pass
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
