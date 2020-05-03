f419ee2abd2012cc4fa27c662aa6051180fc98d2
Jisun Park <fpdldk912@gmail.com>
2020-05-03 18:06:05 +0900

    define pagination

--- links/schema.py
+++ links/schema.py
@@ -19,18 +19,31 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String()) # add search param
+    links = graphene.List(
+        LinkType, 
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    ) # add search param, and pagination param:frist, skip
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+        
         if search:
             filter = (
                 Q(url__icontains=search) |
                 Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
-        
-        return Link.objects.all() # return all the links
+            qs = qs.filter(filter)
+
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
