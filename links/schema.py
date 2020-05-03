f6ba465be96fbb33b9a46b92951d2e88c4cc8de2
Jisun Park <fpdldk912@gmail.com>
2020-05-03 14:45:45 +0900

    make Vote models and links to Link

--- links/schema.py
+++ links/schema.py
@@ -2,6 +2,8 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Link
+from users.schema import UserType
+from links.models import Link, Vote
 
 
 class LinkType(DjangoObjectType):
@@ -9,11 +11,20 @@ class LinkType(DjangoObjectType):
         model = Link
 
 
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
+
+
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
+    votes = graphene.List(VoteType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all() # return all the links
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 
 # defines a mutation class
@@ -21,6 +32,7 @@ class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     description = graphene.String()
+    posted_by = graphene.Field(UserType)
 
     # defines the data you can send to the server
     class Arguments:
@@ -39,9 +51,35 @@ class CreateLink(graphene.Mutation):
             id = link.id,
             url = link.url,
             description = link.description,
+            posted_by = link.posted_by
         )
+
+
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    link = graphene.Field(LinkType)
+
+    class Arguments:
+        link_id = graphene.Int()
+
+    def mutate(self, info, link_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('You must be logged to vote!')
+
+        link = Link.objects.filter(id=link_id).first()
+        if not link:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user = user,
+            link = link,
+        )
+
+        return CreateVote(user=user, link=link)
 
 
 # creates a mutation class with a field to be resolved
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
+    create_vote = CreateVote.Field()
