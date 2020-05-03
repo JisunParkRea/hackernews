96f02807e234336945585d43aeaa6800909f6002
Jisun Park <fpdldk912@gmail.com>
2020-05-03 20:46:12 +0900

    make relay

--- conf/schema.py
+++ conf/schema.py
@@ -3,15 +3,26 @@ import graphql_jwt # implement JWT TOkens for user authentication
 
 import links.schema
 import users.schema
+import links.schema_relay
 
 
 # inherits the query in links/schema.py
 # to keep every schema isolated in apps
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    users.schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType,
+):
     pass
 
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    users.schema.Mutation,
+    links.schema.Mutation,
+    links.schema_relay.RelayMutation, 
+    graphene.ObjectType,
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
